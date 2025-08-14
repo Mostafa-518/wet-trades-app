50695efd840c420fd752ac71ba6939db8ecf1453
Mostafa Ashraf <125249957+Mostafa-518@users.noreply.github.com>
2025-08-14 07:27:06 +0000

    Deleting src & supabase

--- supabase/migrations/20250615002715-080a7092-e542-4316-b6f2-485ab2dff493.sql
+++
@@ -1,7 +0,0 @@
-
--- Remove the columns from the projects table
-ALTER TABLE public.projects 
-DROP COLUMN description,
-DROP COLUMN start_date,
-DROP COLUMN end_date,
-DROP COLUMN status;
--- supabase/migrations/20250615022531-8a6ead50-df55-4cd4-a692-107a4bf90231.sql
+++
@@ -1,3 +0,0 @@
-
--- Remove the name column from the subcontractors table since we'll use company_name as the primary identifier
-ALTER TABLE public.subcontractors DROP COLUMN name;
